740c04d864fa225fb3d68e1030a86b503783063c
Yanhao <yanhao@golfgreenshow.com>
2017-06-26 18:08:13 +0800

    所有冲突已解决

--- src/components/TeamInfo.vue
+++ src/components/TeamInfo.vue
@@ -1,52 +1,52 @@
 <template>
   <div>
     <div id="header">
-      <div class="header">
+      <div class="header6">
         <ul class="row">
           <li v-if="false" class="back"></li>
         </ul>
       </div>
       <div class="box_radius" id="bg_height">
-        <div class="bg_logo">
-          <img :src="Logo">
+        <div class="bg_logo team_logo">
+          <img :src="'/static/logo_header.png'">
         </div>
       </div>
     </div>
-    <section id="section">
+    <div id="section">
       <div>
         <ul class="row">
-          <li class="col">
+          <li class="col activer_right">
             <b></b>
           </li>
-          <li class="">
+          <li class="activer_padding10">
             球队简介
           </li>
-          <li class="col">
+          <li class="col activer_left">
             <b></b>
           </li>
         </ul>
       </div>
-      <div class="bgColor" id="bgColor">
+      <div class="bgColor5" id="bgColor">
         <ul class="row">
-          <li class="padding_t" id="padding_t">
+          <li class="activer" id="padding_t">
             <div>
               <div></div>
             </div>
           </li>
-          <li class="">
+          <li class="activer">
             成立时间
           </li>
-          <li class="col">
+          <li class="col activer">
             2008年7月13日
           </li>
         </ul>
         <ul class="row">
-          <li class="padding_t" id="padding_t">
+          <li class="activer" id="padding_t">
             <div>
               <div></div>
             </div>
           </li>
-          <li class="">
+          <li class=" activer">
             现任队长
           </li>
           <li class="col">
@@ -56,12 +56,12 @@
       </div>
       <div class="width">
         <ul class="row">
-          <li class="padding_t" id="padding_t">
+          <li class="activer" id="padding_t">
             <div>
               <div></div>
             </div>
           </li>
-          <li class="padding_t_text" id="padding_t_text">
+          <li class="padding_t_text activer" id="padding_t_text" style="color: #919191">
             球队宗旨
           </li>
           <div class="col padding_t_text" id="padding_t_text">
@@ -72,7 +72,7 @@
       <div class="button">
         球队章程
       </div>
-    </section>
+    </div>
   </div>
 </template>
 
@@ -83,7 +83,6 @@ export default {
   name: 'TeamInfo',
   data () {
     return {
-      Logo: '/static/img/logo.png',
       Caption: ''
     }
   },
@@ -105,20 +104,12 @@ export default {
   },
 
   mounted () {
-    setTimeout(function () {
-      var winHeight = window.screen.height
-      document.getElementById('header').style.height = winHeight / 3 + 'px'
-      document.getElementById('section').style.height = winHeight - winHeight / 3 + 'px'
-      document.getElementById('bgColor').style.lineHeight = (winHeight - (winHeight / 3)) / 7 + 'px'
-      document.getElementById('padding_t').style.height = (winHeight - (winHeight / 3)) / 90 + '%'
-      document.getElementById('padding_t_text').style.height = (winHeight - (winHeight / 3)) / 70 + '%'
-      console.debug(`WinHeight= ${winHeight}`)
-    }, 500)
+
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-@import '../css/infro_frame.css';
+@import '../css/reset.css';
 </style>
